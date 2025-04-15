3439b9f34d5aec952eed1dbd89e0350f113f4e9b
ChangMin59 <dy2ghks@gmail.com>
2025-04-15 11:15:05 +0900

    입력받은 값 출력

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        int score =scanner.nextInt();
+
+        if (score >=90){
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        }
+
     }
 }
